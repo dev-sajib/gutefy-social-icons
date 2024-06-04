bab98eb8f61ce75f84ec7b24fcd23f4240ecfd97
Syed Barekul Islam <56638411+Syed18103392@users.noreply.github.com>
2024-06-04 10:41:49 +0600

    Delete build directory

--- build/customizer.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'e5331e6ed610f6d70651');
--- build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'wp-element', 'wp-i18n'), 'version' => '8f7dd042b55442de5ae5');
